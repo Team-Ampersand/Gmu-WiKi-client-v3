5b926353e9d023a738166fd5ffb8866a3c92b6f0
한준 <s22073@gsm.hs.kr>
2023-08-11 23:13:20 +0900

    💄 : Update globalStyle

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,11 @@
-import '@/styles/globals.css'
+import Globalstyle from '@/styles/globals'
 import type { AppProps } from 'next/app'
 import { RecoilRoot } from 'recoil'
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <RecoilRoot>
+      <Globalstyle />
       <Component {...pageProps} />
     </RecoilRoot>
   )
