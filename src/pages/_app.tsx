3f374c72327f7e7ef3c7aa80a456210c85f8529f
한준 <s22073@gsm.hs.kr>
2023-08-12 12:48:23 +0900

    🎨 : import Header structure

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,3 +1,4 @@
+import Header from '@/components/Header'
 import Globalstyle from '@/styles/globals'
 import type { AppProps } from 'next/app'
 import { RecoilRoot } from 'recoil'
@@ -6,6 +7,7 @@ export default function App({ Component, pageProps }: AppProps) {
   return (
     <RecoilRoot>
       <Globalstyle />
+      <Header />
       <Component {...pageProps} />
     </RecoilRoot>
   )
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,4 +1,3 @@
-import Header from '@/components/Header'
 import { Head, Html, Main, NextScript } from 'next/document'
 
 export default function Document() {
@@ -6,7 +5,6 @@ export default function Document() {
     <Html lang='en'>
       <Head />
       <body>
-        <Header />
         <Main />
         <NextScript />
       </body>
