fa669ed7d95f5a688dd23c258922de6cde9d983d
한준 <106512781+apnalkkamkkamhanjoon@users.noreply.github.com>
2023-08-13 10:48:48 +0900

    Revert "🔀 Header 퍼블리싱"

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,13 +1,10 @@
-import Header from '@/components/Header'
-import Globalstyle from '@/styles/globals'
+import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
 import { RecoilRoot } from 'recoil'
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <RecoilRoot>
-      <Globalstyle />
-      <Header />
       <Component {...pageProps} />
     </RecoilRoot>
   )
--- src/types/components/DropMenuPropsTypes.ts
+++
@@ -1,4 +0,0 @@
-export interface DropMenuPropsType {
-  onMouseEnter: () => void
-  onMouseLeave: () => void
-}
