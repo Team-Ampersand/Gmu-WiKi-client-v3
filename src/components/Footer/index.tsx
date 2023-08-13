8c0b02dab5620e91c2625db3689c43cce3b8f009
unknown <momlin069@gmail.com>
2023-08-13 14:58:42 +0900

    💄: Add Footer

---
+++ src/components/Footer/index.tsx
@@ -0,0 +1,11 @@
+import * as S from './style'
+
+function Footer() {
+  return (
+    <S.FooterContainer>
+      <p>Copyright 2023. &mpersand All rights reserved.</p>
+    </S.FooterContainer>
+  )
+}
+
+export default Footer
